c0cc6383474637a40f7af0bbdaaa122353a73368
Alberto Ricatto s309141 <s309141@studenti.polito.it>
2024-01-08 22:19:59 +0100

    made imports work

--- src/agent.py
+++ src/agent.py
@@ -1,7 +1,10 @@
 import numpy as np                                      # Umbi so che non ti piace ma 
 from random import choice
 from oracle import Oracle as ocl
-from ..lib.game import Game, Move, Player
+
+import sys                                              # Umbi probabilmente neanche questo ti piace ma
+sys.path.append('../')
+from quixo.lib.game import Game, Move, Player
 
 Position = tuple[int, int]
 
@@ -17,15 +20,15 @@ def getPossibleMoves(board: np.ndarray) -> list[tuple[Position, Move]]:
 class DelphiPlayer(Player):
     def __init__(self) -> None:
         super().__init__()
-        ocl: self.delphi = ocl() 
-        list[np.ndarray]: self.episode = [np.ndarray]   
+        self.delphi: ocl = ocl() 
+        self.episode: list[np.ndarray] = [np.ndarray]   
 
     # TODO: implement minmax
     def make_move(self, game: 'Game') -> tuple[tuple[int, int], Move]:
-        tuple[Position, Move]: choice = choice(getPossibleMoves(game.get_board()))
+        chosen: tuple[Position, Move] = choice(getPossibleMoves(game.get_board()))
         #implement minmax
-        self.episode.append(choice[0])
-        return choice
+        self.episode.append(chosen[0])
+        return chosen
     
     def feedback(self, won: bool) -> None:
         """at the end of a game, gives feedback to the oracle"""
@@ -39,3 +42,5 @@ if __name__ == '__main__':
     print('helo uze theez 2 test')    
     dp = DelphiPlayer()
     g = Game()
+    g.play(dp, dp)
+    g.print()
