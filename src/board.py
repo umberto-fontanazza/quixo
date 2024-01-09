1afd2b77980722280a2f5b6707888dc95a262f9a
Umberto Fontanazza <umbertofontanazza@MacBook-Pro-di-Umberto.local>
2024-01-09 15:58:33 +0100

    Add random board generator

--- src/board.py
+++ src/board.py
@@ -1,6 +1,10 @@
 from typing import Literal
 from numpy.typing import NDArray
+import numpy as np
 
 Board = NDArray
 Position = tuple[int, int]
 Outcome = Literal['Win', 'Loss']
+
+def random_board() -> Board:
+    return np.random.choice([0, 1, -1], size=(5,5))
