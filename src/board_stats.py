3bd3dd25aea788ecc1f324e90c01812dbf3a2b05
Alberto Ricatto s309141 <s309141@studenti.polito.it>
2024-01-19 10:09:59 +0100

    added board_stats

---
+++ src/board_stats.py
@@ -0,0 +1,36 @@
+from src.board import Board, CompleteMove
+from src.player import PlayerID
+from lib.game import Move
+
+# TODO: define some stats for the board: when you put one in
+class BoardStats:
+    def __init__(self,  board: Board, parent_board: Board | None = None, move: CompleteMove | None = None, player: PlayerID | None = None) -> None:
+        # define stats that have to computed from scratch every time
+        self.x_available_moves = 0              # TODO: add more of these
+        self.o_available_moves = 0              # TODO: make these work
+
+        # define how to compute stats from a parent board
+        if parent_board is not None and move is not None and player is not None:
+            arr_parent = parent_board.ndarray
+            if arr_parent[move[0]] == -1:
+                if player in ('X', 1):
+                    self.x_count = parent_board.stats.x_count + 1
+                else:
+                    self.o_count = parent_board.stats.o_count + 1
+            else:
+                self.o_count, self.x_count = parent_board.stats.o_count, parent_board.stats.x_count
+
+        # define how to compute the SAME stats on a new board
+        else:
+            arr_board = board.ndarray
+            self.x_count = arr_board[arr_board == 1].sum()
+            self.o_count = arr_board[arr_board == 0].sum()
+
+
+    @property
+    def all_stats(self):
+        """get all the stats of the board"""
+        return [
+            self.x_count,
+            self.o_count
+        ]
